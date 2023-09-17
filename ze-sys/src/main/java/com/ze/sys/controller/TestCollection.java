d02e2d3003c3420b4345fe2c78f2381e59f827bf
ZENG <614695876@qq.com>
2023-09-17 21:23:58 +0800

    redis集成

--- ze-sys/src/main/java/com/ze/sys/controller/TestCollection.java
+++ ze-sys/src/main/java/com/ze/sys/controller/TestCollection.java
@@ -1,6 +1,8 @@
 package com.ze.sys.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,15 @@ public class TestCollection {
     @GetMapping("hello")
     private String hello(){
         return "hello";
+    }
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/testRedis")
+    public String testRedis() {
+        redisTemplate.opsForValue().set("name", "倪郝");
+        String name = (String) redisTemplate.opsForValue().get("name");
+        return name;
     }
 }
