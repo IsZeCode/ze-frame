4e6e468eb774a1eb4b50af5ee0091bd48d5b6fe4
ZENG <614695876@qq.com>
2023-09-18 15:30:52 +0800

    分布式锁的实现

--- ze-sys/src/main/java/com/ze/sys/controller/TestCollection.java
+++ ze-sys/src/main/java/com/ze/sys/controller/TestCollection.java
@@ -2,6 +2,7 @@ package com.ze.sys.controller;
 
 
 import com.ze.bean.Result;
+import com.ze.redis.util.RedisShareLockUtil;
 import com.ze.redis.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestCollection {
 
     @GetMapping("hello")
-    private String hello(){
+    private String hello() {
         return "hello";
     }
 
@@ -20,7 +21,17 @@ public class TestCollection {
 
     @GetMapping("/testRedis")
     public Result testRedis() {
-        redisUtil.set("me","则");
+        redisUtil.set("me", "则");
         return Result.ok();
+    }
+
+    @Autowired
+    private RedisShareLockUtil redisShareLockUtil;
+
+    @GetMapping("/testRedisShareLock")
+    public String testRedisShareLock() {
+        boolean result = redisShareLockUtil.lock("qj", "123456", 10000L);
+        System.out.println("分布式锁获取：" + result);
+        return String.valueOf(result);
     }
 }
